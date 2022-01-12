48f7397867c8ba307146694ff55971175ac83fdf
DanWilk <dcwilkinson@wilktech.com>
2022-01-12 12:39:07 -0500

    adjusted file structure

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const fs = require('fs');
+const uniqid = require('uniqid');
 const { notes } = require('../../db/db.json');
-const { createNote, validateNote } = require('../../lib/notes');
+const { createNote, validateNote, deleteNote } = require('../../lib/notes');
 
 router.get('/notes', (req, res) => {
     let results = notes;
@@ -9,12 +10,19 @@ router.get('/notes', (req, res) => {
 });
 
 router.post('/notes', (req, res) => {
-    req.body.id = notes.length.toString()
+    req.body.id = uniqid()
 
     if (!validateNote) {
         res.status(400).send('The Note is not correct');
-    } else note = createNote(req.body, notes);
-    res.json(note);
+    } else notes = createNote(req.body, notes);
+    res.json(notes);
+})
+
+router.delete('/notes:id', (req, res) => {
+    deleteNote(req.params.id, notes);
+
+    let results = notes;
+    res.json(results);
 })
 
 module.exports = router;
